2c2acf4f613cabc1fb1cd005a1e8cdf6b7109c10
Ales Povalac <povalac@vut.cz>
2021-09-23 09:10:57 +0200

    cv01 - morseovka

--- cv01/Src/main.c
+++ cv01/Src/main.c
@@ -30,11 +30,19 @@ int main(void)
 	RCC->AHBENR|=RCC_AHBENR_GPIOAEN;
 	GPIOA->MODER|=GPIO_MODER_MODER5_0;
 
-	/* Loop forever */
 	for(;;)
 	{
-		GPIOA->ODR^=(1<<5);
+		uint32_t pole=0b10101001110111011100101010000000;
 
-		for (volatile uint32_t i=0; i<100000; i++) {}
+	/* Loop forever */
+		for(uint32_t i=0;i<32;i++)
+		{
+			if(pole & (1<<(31-i)))
+				GPIOA->BSRR=(1<<5); //set
+			else
+				GPIOA->BRR=(1<<5); //reset
+
+			for (volatile uint32_t i=0; i<100000; i++) {}
+		}
 	}
 }
