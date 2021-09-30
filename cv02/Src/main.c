7f51d4f076e6af3769d55345ca25d4d3bb679804
Ales Povalac <povalac@vut.cz>
2021-09-30 09:35:34 +0200

    cv02 (zakmity)

--- cv02/Src/main.c
+++ cv02/Src/main.c
@@ -21,7 +21,7 @@
 #include <stm32f0xx.h>
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
-  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
+#warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
 #define LED_TIME_BLINK 300
@@ -29,59 +29,87 @@
 #define LED_TIME_LONG  1000
 
 void EXTI0_1_IRQHandler(void)
- {
- if (EXTI->PR & EXTI_PR_PR0) { // check line 0 has triggered the IT
- EXTI->PR |= EXTI_PR_PR0; // clear the pending bit
- GPIOB->ODR ^= (1 << 0);
- }
- }
+{
+	if (EXTI->PR & EXTI_PR_PR0) { // check line 0 has triggered the IT
+		EXTI->PR |= EXTI_PR_PR0; // clear the pending bit
+		GPIOB->ODR ^= (1 << 0);
+	}
+}
 
 volatile uint32_t Tick;
+
 void SysTick_Handler(void)
- {
- Tick++; //period 1ms
- }
+{
+	Tick++; //period 1ms
+}
 
 void blikac(void)
- {
- static uint32_t delay;
+{
+	static uint32_t delay;
 
- if (Tick > delay + LED_TIME_BLINK) {
- GPIOA->ODR ^= (1 << 4);
- delay = Tick;
- }
- }
+	if (Tick > delay + LED_TIME_BLINK) {
+		GPIOA->ODR ^= (1 << 4);
+		delay = Tick;
+	}
+}
 
 void tlacitka(void)
- {
- static uint32_t old_s2;
- static uint32_t off_time;
- uint32_t new_s2 = GPIOC->IDR & (1<<0);
+{
+	static uint32_t old_s2;
+	static uint32_t off_time;
+	static uint16_t debounce = 0xFFFF;
+	static uint32_t shift_time;
+	// static uint32_t old_s1;
 
- static uint32_t old_s1;
- uint32_t new_s1 = GPIOC->IDR & (1<<1);
 
- if (old_s2 && !new_s2) { // falling edge
-	 off_time = Tick + LED_TIME_SHORT;
-	 GPIOB->BSRR = (1<<0);
- }
+	uint32_t new_s2 = GPIOC->IDR & (1<<0);
+	if (old_s2 && !new_s2) // falling edge
+	{
+		off_time = Tick + LED_TIME_SHORT;
+		GPIOB->BSRR = (1<<0);
+	}
 
-  if (Tick > off_time) {
-	  GPIOB->BRR = (1<<0);
-  }
 	old_s2 = new_s2;
 
-
-  if (old_s1 && !new_s1) { // falling edge
+	/*  uint32_t new_s1 = GPIOC->IDR & (1<<1);
+  if (old_s1 && !new_s1)  // falling edge
+  	  {
 	  off_time = Tick + LED_TIME_LONG;
 	  GPIOB->BSRR = (1<<0);
 	 }
 
-  if (Tick > off_time) {
+  if (Tick > off_time)
+  	  {
 	  GPIOB->BRR = (1<<0);
 	  }
 		old_s1 = new_s1;
+	 */
+
+	if (Tick > shift_time + 5)
+	{
+		debounce <<= 1;
+		if (GPIOC->IDR & (1 << 1))
+		{
+			debounce |= 0x0001;
+		}
+		if (debounce == 0x7FFF)
+		{
+			GPIOB->BSRR = (1<<0);
+			off_time = Tick + LED_TIME_LONG;
+		}
+		shift_time = Tick;
+	}
+
+
+	if (Tick > off_time)
+	{
+		GPIOB->BRR = (1<<0);
+	}
 }
+
+
+
+
 
 int main(void)
 {
@@ -97,11 +125,11 @@ int main(void)
 	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
 
 	//External interrupt
-/*	SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PC; // select PC0 for EXTI0
+	/*	SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PC; // select PC0 for EXTI0
 	EXTI->IMR |= EXTI_IMR_MR0; // mask
 	EXTI->FTSR |= EXTI_FTSR_TR0; // trigger on falling edge
 	NVIC_EnableIRQ(EXTI0_1_IRQn); // enable EXTI0_1
-*/
+	 */
 	while (1) {
 		blikac();
 		tlacitka();
